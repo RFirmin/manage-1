fb32549ceed223246266729070437ffbbb12c642
Firmin <flinsraiderk7@gmail.com>
2024-02-14 13:06:48 +0100

    add the templates and static files

--- enter/models.py
+++ enter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db.models.signals import pre_save
+
 from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
