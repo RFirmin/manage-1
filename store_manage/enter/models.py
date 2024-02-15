9acb29fb59accbb61ed9df0ef455a36e475340ff
Firmin <flinsraiderk7@gmail.com>
2024-02-15 13:50:14 +0100

    work about category, equipment and their detail

--- store_manage/enter/models.py
+++ store_manage/enter/models.py
@@ -1,6 +1,7 @@
 from django.db import models
-from django.db.models.signals import pre_save
+
 from django.dispatch import receiver
+from django.db.models.signals import pre_save
 
 # Create your models here.
 
@@ -9,7 +10,7 @@ class Category(models.Model):
     slug = models.SlugField(max_length=100, unique=True, blank=True)
     description = models.TextField(blank=True, verbose_name="Description")
     number = models.IntegerField(blank=True, default=0, verbose_name="Quantity_of_equipment")
-    image = models.ImageField(upload_to='enter_image/', null=True, blank=True)
+    image = models.ImageField(upload_to='enter_category/', blank=True, null=True)
 
     class Meta:
         verbose_name = "Category_Equipment"
@@ -29,6 +30,7 @@ class Equipment(models.Model):
     classed = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Category_of_equipement")
     state = models.BooleanField(default=True)
     number = models.IntegerField()
+    image = models.ImageField(upload_to='enter_equipment/', blank=True, null=True)
 
     class Meta:
         verbose_name = "Equipment_stored"
--- store_manage/enter/views.py
+++ store_manage/enter/views.py
@@ -4,6 +4,8 @@ from .models import Category, Equipment
 
 from .forms import CategoryForm, EquipmentForm
 
+from django.contrib.auth.decorators import user_passes_test
+
 from django.core.paginator import Paginator
 
 # Create your views here.
@@ -13,12 +15,13 @@ def screen_category(request):
     enter_category = Category.objects.all()
 
     context = {
-
+        'equipments' : enter_category,
     }
 
     return render(request, "all_category.html", context)
 
 
+@user_passes_test(lambda u: "master" in [group.name for group in u.groups.all()])
 def screen_equipment(request):
     
     enter_equipment = Equipment.objects.all()
@@ -29,8 +32,18 @@ def screen_equipment(request):
     page_object = paginator.get_page(page_number)
 
     context = {
-
+        'equipments' : page_object,
     }
 
     return render(request, "all_equipment.html", context)
 
+
+def details_equipment(request, serial_number):
+    
+    details = Equipment.objects.get(number=serial_number)
+    
+    context = {
+        'details' : details
+    }
+    
+    return render(request, 'details_equipment.html', context)
