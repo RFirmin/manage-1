0d4d8176b1e8ba8b32b053ee111b01e7de9fad60
NTONGA FRANCK LOÏC <nloichack@gmail.com>
2024-03-19 23:57:35 +0100

    Edited the entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,8 @@
 #Had too much problems with the database
 set -e
 
+echo "Running migrations..."
 python store_manage/manage.py migrate
 
-if [ "$l" == "gunicorn" ]; then
-    exec gunicorn store_manage.wsgi:application -b 0.0.0.0:8000
-else
-    exec python store_manage/manage.py runserver 0.0.0.0:8000
-fi
+echo "Starting Gunicorn..."
+exec gunicorn store_manage.wsgi:application -b 0.0.0.0:8000
