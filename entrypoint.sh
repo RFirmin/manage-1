3be1d867bfe1459314343b5c80ef171d139c12a8
NTONGA FRANCK LOÏC <nloichack@gmail.com>
2024-03-20 03:10:55 +0100

    Edited the entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,4 @@ set -e
 
 echo "Running migrations..."
 python store_manage/manage.py migrate
-
-echo "Starting Gunicorn..."
-exec gunicorn store_manage.wsgi:application -b 0.0.0.0:8000
+python store_manage/manage.py runserver
