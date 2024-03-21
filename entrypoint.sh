744ce36f599650366101f78936a6c9ada31d59a2
NTONGA FRANCK LOÏC <nloichack@gmail.com>
2024-03-21 06:13:39 +0100

    setings fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 python store_manage/manage.py makemigrations
 python store_manage/manage.py migrate
 
-if [ "$l" == 'gunicorn']; then
+if [ "$l" == 'gunicorn' ]; then
 
     exec gunicorn store_manage.wsgi:application -b 0.0.0.0:$WEBSITES_PORT
 
