e756f26df48918bc1207a077354a533fc9d41633
NTONGA FRANCK LOÏC <nloichack@gmail.com>
2024-03-21 07:30:26 +0100

    wsgi error

--- store_manage/store_manage/wsgi.py
+++ store_manage/store_manage/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_manage.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'store_manage.store_manage.settings')
 
 application = get_wsgi_application()
