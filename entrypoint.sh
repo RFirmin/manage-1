65beb03117787c7d3fed1d832cd6508a2a873363
NTONGA FRANCK LOÏC <nloichack@gmail.com>
2024-03-21 05:07:08 +0100

    Changes n° xxxx97677988652

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,10 +7,10 @@ python manage.py migrate
 
 if [$l == 'gunicorn']; then
 
-    exec gunicorn store_manage.wsgi:application -b 0.0.0.0:8000
+    exec gunicorn store_manage.wsgi:application -b 0.0.0.0:$WEBSITES_PORT
 
 else
 
-    exec python manage.py runserver 0.0.0.0:8000
+    exec python manage.py runserver 0.0.0.0:$WEBSITES_PORT
 
 fi
